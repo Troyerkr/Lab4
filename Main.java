3fdba48cf9978f4ded0cd90fdfb86fd360a77976
Troyerkr <krobtroyer@gmail.com>
2021-02-09 03:09:11 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+/*import java.util.Scanner;*/
+
+class Main {
+  public static void main(String[] args) {
+    Random r = new Random();
+    int randomNum = r.nextInt(50) + 1;
+    System.out.println("The random number is: " + randomNum);
+    int lastCheck = randomNum;
+
+    //I just reused my code from lab 3
+    System.out.println(randomNum);
+    
+      while (randomNum != 0) {
+        randomNum = randomNum - 1;
+        System.out.println(randomNum);
+      }
+
+    if(lastCheck <= 15 ){
+      System.out.println("Ahoy mateys!");
+    } else if(lastCheck > 20 & lastCheck <42){
+      System.out.println("Cannonball!");
+    }else{
+      System.out.println("Look, a sea shanty!");
+    }
+
+  }
+}
